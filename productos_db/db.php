44c0950ceb140002bfe93f582cc7140c62174a3e
Luis José Ruiz <155862761+Russohhp@users.noreply.github.com>
2025-02-15 09:50:20 -0400

    Update db.php

--- productos_db/db.php
+++ productos_db/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'productos_db';
 $username = 'root';
-$password = 'hola123';  // Cambié la contraseña aquí
+$password = '';  
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
